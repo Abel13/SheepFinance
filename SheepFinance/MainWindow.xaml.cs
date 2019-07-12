0cbf9940c666e9e93db8a0726d96f4a20507e4a5
abel <abel.o.d@outlook.com>
2019-07-12 01:02:35 -0300

    versão 4.1.0.3

--- SheepFinance/MainWindow.xaml.cs
+++ SheepFinance/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,7 @@ namespace SheepFinance
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = Assembly.GetExecutingAssembly().GetName().Version.ToString();
             control = new ControlMain();
             ListViewMenu.ItemsSource = control.GetMenuList();
             control.GetDollaresAPI();
